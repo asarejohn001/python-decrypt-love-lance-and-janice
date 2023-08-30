84c457bff948a8426faf31235ac278cf8eb52d65
John Asare <137245223+asarejohn001@users.noreply.github.com>
2023-08-30 00:31:53 -0400

    update solution.py: add a for loop to get the index of the encrypted message and use that index to retrieve the corresponding letter from the normal alphabet.

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ import string
 
 
 # defined a function to hold the solution
-def solution():
+def solution(encrypted_message):
     # get the alphabets from the string library.
     alphabet_list = list(string.ascii_lowercase)
     # print as a check-up to make sure the alphabet are stored as list.
@@ -21,5 +21,10 @@ def solution():
     # print as a check point to make the alphabet are indeed reversed.
     print(alphabet_reversed)
 
+    for letter in encrypted_message:
+        encrypted_letter_index = alphabet_reversed.index(letter)
+        decrypted_letter = alphabet_list[encrypted_letter_index]
+        print(decrypted_letter)
 
-solution()
+
+solution("vmxibkgrlm P . ?")
