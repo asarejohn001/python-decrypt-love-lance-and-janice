c17a95f9f7235d40bbae533a8e3cf3704e7dc2c2
John Asare <137245223+asarejohn001@users.noreply.github.com>
2023-08-30 00:09:40 -0400

    update solution.py. add alphabet list and reversed alphate line of the code.

--- solution.py
+++ solution.py
@@ -10,9 +10,16 @@ Read the README.md file for a detailed description of the project.
 import string
 
 
+# defined a function to hold the solution
 def solution():
+    # get the alphabets from the string library.
     alphabet_list = list(string.ascii_lowercase)
+    # print as a check-up to make sure the alphabet are stored as list.
     print(alphabet_list)
+    # reversed the alphabet and make it as a list.
+    alphabet_reversed = list(reversed(alphabet_list))
+    # print as a check point to make the alphabet are indeed reversed.
+    print(alphabet_reversed)
 
 
 solution()
