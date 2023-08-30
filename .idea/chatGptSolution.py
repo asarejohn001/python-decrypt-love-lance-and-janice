30590f44f67718f94373debbcd24b4600853e217
John Asare <137245223+asarejohn001@users.noreply.github.com>
2023-08-30 01:46:15 -0400

    add a new python file. The file contain chat GPT version to tackle the solution. There is no need to learn python at this point.

---
+++ .idea/chatGptSolution.py
@@ -0,0 +1,16 @@
+def solution(s):
+    deciphered = ''
+
+    for char in s:
+        if 'a' <= char <= 'z':
+            deciphered += chr(ord('z') - (ord(char) - ord('a')))
+        else:
+            deciphered += char
+
+    return deciphered
+
+
+# Example usage
+coded_message = "vmxibkgrlm"
+decoded_message = solution(coded_message)
+print(decoded_message)  # Output will be: "encryption"
