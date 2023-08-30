4ef23855d7aad5c7d6dc5b472acf2660a254c861
John Asare <137245223+asarejohn001@users.noreply.github.com>
2023-08-29 20:53:20 -0400

    create new py file and add solution.py function.

---
+++ solution.py
@@ -0,0 +1,18 @@
+"""
+Author: John Asare
+Date and time: 2023-08-09 20:47
+
+Read the README.md file for a detailed description of the project.
+
+"""
+
+# import the string file
+import string
+
+
+def solution():
+    alphabet_list = list(string.ascii_lowercase)
+    print(alphabet_list)
+
+
+solution()
